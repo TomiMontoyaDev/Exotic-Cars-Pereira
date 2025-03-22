41374904caf93b00d2b4b9537e52105a1f4b5d30
TomiMontoya <tomasmontoyabuitrago@gmail.com>
2025-03-22 16:15:43 -0500

    Se agregaron nuevos carros

--- carros.js
+++ carros.js
@@ -47,6 +47,62 @@ const carros = [
     imagen: "./Carrosfotos/Carros Pagina/MX5 blue/Mx5Blue.jpg",
   },
   /* FIN CARRO 7 */
+
+  /* CARRO 8 */
+  {
+    nombre: "Chevrolet Camaro SS",
+    imagen: "./Carrosfotos/Carros Pagina/Camaro SS YELLOW/Camaroyellow.jpg",
+  },
+  /* FIN CARRO 8 */
+
+  /* CARRO 9 */
+  {
+    nombre: "BMW 125i Coupe",
+    imagen: "./Carrosfotos/Carros Pagina/BMW 125i Coupe/IMG_4919.jpg",
+  },
+  /* FIN CARRO 9 */
+
+  /* CARRO 10 */
+  {
+    nombre: "Mercedes Benz A200",
+    imagen: "./Carrosfotos/Carros Pagina/Mercedes A200/IMG_6361.jpg",
+  },
+  /* FIN CARRO 10 */
+
+  /* CARRO 11 */
+  {
+    nombre: "BMW 320i F30",
+    imagen: "./Carrosfotos/Carros Pagina/BMW 320i F30/IMG_9474.jpg",
+  },
+  /* FIN CARRO 11 */
+
+  /* CARRO 12 */
+  {
+    nombre: "BMW 420i F33 Cabriolet",
+    imagen: "./Carrosfotos/Carros Pagina/BMW420i/IMG_9502.jpg",
+  },
+  /* FIN CARRO 12 */
+
+  /* CARRO 13 */
+  {
+    nombre: "BMW 420i F36 Grand Coupe",
+    imagen: "./Carrosfotos/Carros Pagina/BMW 420i F36/IMG_0942.jpg",
+  },
+  /* FIN CARRO 13 */
+
+  /* CARRO 14 */
+  {
+    nombre: "Porsche Taycan S",
+    imagen: "./Carrosfotos/Carros Pagina/PorscheTaycan/taycan.jpg",
+  },
+  /* FIN CARRO 14 */
+
+  /* CARRO 15 */
+  {
+    nombre: "BMW M4 Competition",
+    imagen: "./Carrosfotos/Carros Pagina/BMW M4/m4.jpg",
+  },
+  /* FIN CARRO 15 */
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -54,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   carros.forEach((carro) => {
     const card = document.createElement("div");
-    card.classList.add("col-md-4");
+    card.classList.add("col-md-4", "mb-4");
 
     card.innerHTML = `
         <div class="card h-100 bg-dark mb-3 border-light">
